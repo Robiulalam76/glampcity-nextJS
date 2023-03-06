5539f46aaceacb9314fb70a530b0ef04bfb10ccd
robiulalam76 <robiulalam762400@gmail.com>
2023-03-06 13:44:32 +0600

    customer support page complete

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import bannerSlice from "@/Slices/bannerSlice";
 import controllerSlice from "@/Slices/controllerSlice";
+import customerSupportSlice from "@/Slices/customerSupportSlice";
 import { combineReducers } from "@reduxjs/toolkit";
 
 const rootReducer = combineReducers({
   controllerSlice: controllerSlice,
   bannerSlice: bannerSlice,
+  customerSupportSlice: customerSupportSlice,
 });
 
 export default rootReducer;
